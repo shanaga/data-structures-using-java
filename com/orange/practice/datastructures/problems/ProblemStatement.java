61bd929b3bad4f45a42f81938211cc2dfc3b3e28
Shantanu Agarwal <shantanu.agarwal2109@gmail.com>
2019-10-05 00:47:14 -0400

    Implement to sort the list by the second character of the last name

--- com/orange/practice/datastructures/problems/ProblemStatement.java
+++ com/orange/practice/datastructures/problems/ProblemStatement.java
@@ -5,46 +5,31 @@ import java.util.*;
 public class ProblemStatement {
 
     // Input : Sachin Tendulkar, Rahul Dravid, Saurav Ganguly, Shane Warna
-    // Output : Sorted based on Second character in the string
+    // Output : Saurav GAnguly, Shane WArne, Sachin TEndulkar, Rahul DRavid
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        list.add("Sachin Tendulkar");
-        list.add("Rahul Dravid");
-        list.add("Saurav Ganguly");
-        list.add("Shane Warne");
-        sortUsingFirstCharacter(list);
+        list.add("Ravinder Jadeja");
+        list.add("MS Dhoni");
+        list.add("Virat Kohli");
+        list.add("Rohit Sharma");
+        sortUsingSecondCharacter(list);
     }
 
-
-    private static void sortUsingFirstCharacter(List<String> inputList) {
-        // Java 7 implementation
-        /*Collections.sort(inputList, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String[] split1 = o1.split(" ");
-                String[] split2 = o2.split(" ");
-                String lastName1 = split1[1];
-                String lastName2 = split2[1];
-
-                if (lastName1.compareTo(lastName2) > 0) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            }
-        });
-        System.out.println(inputList);*/
-
-
+    /*
+      Method to sort the list by the second character of the last name
+     */
+    private static void sortUsingSecondCharacter(List<String> inputList) {
         // Java 8 implementation
         Collections.sort(inputList, (o1, o2) -> {
             String[] split1 = o1.split(" ");
             String[] split2 = o2.split(" ");
             String lastName1 = split1[1];
             String lastName2 = split2[1];
+            char c1 = lastName1.charAt(1);
+            char c2 = lastName2.charAt(1);
 
-            if (lastName1.compareTo(lastName2) > 0) {
+            if (c1 > c2) {
                 return 1;
             } else {
                 return -1;
