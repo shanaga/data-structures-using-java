313b543e0e6d197d83b76668bcc86b53a6a1aa42
Shantanu Agarwal <shantanu.agarwal2109@gmail.com>
2019-10-05 00:53:44 -0400

    Further Changes

--- com/orange/practice/datastructures/problems/ProblemStatement.java
+++ com/orange/practice/datastructures/problems/ProblemStatement.java
@@ -4,23 +4,27 @@ import java.util.*;
 
 public class ProblemStatement {
 
-    // Input : Sachin Tendulkar, Rahul Dravid, Saurav Ganguly, Shane Warna
-    // Output : Saurav GAnguly, Shane WArne, Sachin TEndulkar, Rahul DRavid
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("Ravinder Jadeja");
-        list.add("MS Dhoni");
-        list.add("Virat Kohli");
-        list.add("Rohit Sharma");
-        sortUsingSecondCharacter(list);
+        // Initialize players list by adding players
+        List<String> players = initializePlayersList();
+        // Sort list using last name's second character
+        sortListUsingLastNameSecondCharacter(players);
+    }
+
+    static List<String> initializePlayersList() {
+        List<String> players = new ArrayList<>();
+        players.add("Ravinder Jadeja");
+        players.add("MS Dhoni");
+        players.add("Virat Kohli");
+        players.add("Rohit Sharma");
+        return players;
     }
 
     /*
       Method to sort the list by the second character of the last name
      */
-    private static void sortUsingSecondCharacter(List<String> inputList) {
-        // Java 8 implementation
+    private static void sortListUsingLastNameSecondCharacter(List<String> inputList) {
         Collections.sort(inputList, (o1, o2) -> {
             String[] split1 = o1.split(" ");
             String[] split2 = o2.split(" ");
@@ -35,7 +39,7 @@ public class ProblemStatement {
                 return -1;
             }
         });
-        System.out.println(inputList);
+        System.out.println("Sorted List : " + inputList);
     }
 
 }
